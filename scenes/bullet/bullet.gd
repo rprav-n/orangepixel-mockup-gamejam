a859e9e7df55ac2503f6bddecef2c532be4c1dc1
Praveen <rpraveen.engineer@gmail.com>
2023-10-25 21:35:47 +0530

    Clear bullets off screen

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -5,10 +5,11 @@ extends Area2D
 const MAX_SPEED: int = 400
 
 var direction: Vector2 = Vector2.RIGHT
+@onready var notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
-
+	notifier.connect("screen_exited", Callable(self, "_on_screen_exited"))
 
 func _process(delta: float):
 	global_position += direction * MAX_SPEED * delta
@@ -17,3 +18,7 @@ func _process(delta: float):
 func _on_body_entered(body: Node2D):
 	if body.is_in_group("terrain_group"):
 		queue_free()
+
+
+func _on_screen_exited():
+	queue_free()
